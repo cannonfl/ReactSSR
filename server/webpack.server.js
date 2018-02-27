7fb345344517eb95fadfb709ef763fbb19004522
Frank Cannon <cannonfl@gmail.com>
2018-02-27 13:16:11 -0800

    merge webpack configs

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,30 +1,17 @@
 const path = require('path');
+const merge = require("webpack-merge");
+const baseConfig = require("./webpack.base.js");
 
-module.exports = {
+ const config = {
   // building a bundle for node.js, not browser
   target: 'node',
 
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: '/node-modules/',
-        options: {
-          presets: [
-            'react', 
-            'stage-0',
-            ['env', {targets: {browsers: ['last 2 versions']}}]
-          ]
-        }
-      }
-    ]
+    path: path.resolve(__dirname, 'build'),
   }
-
+  
 };
+
+module.exports = merge(baseConfig, config);
